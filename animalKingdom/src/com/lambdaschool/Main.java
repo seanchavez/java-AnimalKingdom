dcc6f2ed4f1053cb7044c7615cbcadd772af9a6f
Sean Chavez <realseanchavez@gmail.com>
2019-02-07 18:25:54 -0800

    add animals

--- animalKingdom/src/com/lambdaschool/Main.java
+++ animalKingdom/src/com/lambdaschool/Main.java
@@ -7,6 +7,26 @@ public class Main {
 
         ArrayList<Animals> animals = new ArrayList<Animals>();
         animals.add(new Mammals("Panda", 1869));
-        System.out.println(animals);
+        animals.add(new Mammals("Panda", 1778));
+        animals.add(new Mammals("Koala", 1816));
+        animals.add(new Mammals("Sloth", 1804));
+        animals.add(new Mammals("Armadillo", 1758));
+        animals.add(new Mammals("Raccoon", 1758));
+        animals.add(new Mammals("Bigfoot", 2021));
+
+        animals.add(new Birds("Pigeon", 1837));
+        animals.add(new Birds("Peacock", 1821));
+        animals.add(new Birds("Toucan", 1758));
+        animals.add(new Birds("Parrot", 1824));
+        animals.add(new Birds("Swan", 1758));
+
+        animals.add(new Fish("Salmon", 1758));
+        animals.add(new Fish("Catfish", 1758));
+        animals.add(new Fish("Perch", 1758));
+
+        for (Animals animal : animals) {
+            System.out.println(animals.getName());
+        }
+
     }
 }
